b9fd7e35ee3def76b8f4a7c5774f345cdad5cc7e
Laudwika <70614573+Laudwika@users.noreply.github.com>
2023-07-26 15:12:24 +0900

    Update generate.py

--- generate.py
+++ generate.py
@@ -27,6 +27,7 @@ from skimage import io, img_as_float32
 
 from networks.customspade import CustomSPADE
 from networks.generator import G
+from networks.tex_generator import G_tex
 from SPADE.options.train_options import TrainOptions
 
 from model import Encoder as Encoder, Generator as Generator_style
@@ -195,13 +196,20 @@ segmentation_model.to(device)
 
 ###########################################################
 #########################TEXTURE###########################
-texture_model = CustomSPADE(num_classes=20)
-texture_model.load_state_dict(torch.load('models/usable.pt', map_location=device))
+# texture_model = CustomSPADE(num_classes=20)
+# texture_model.load_state_dict(torch.load('models/usable.pt', map_location=device))
+# texture_model.eval()
+# # texture_model = nn.DataParallel(texture_model)
+
+# texture_model.to(device)
+
+
+###########################################################
+####################TEXTURE ADAIN##########################
+texture_model = G_tex(vgg_model)
+texture_model.load_state_dict(torch.load('models/encoder_10.pt'))
 texture_model.eval()
-# texture_model = nn.DataParallel(texture_model)
-
-texture_model.to(device)
-
+texture_model = texture_model.cuda()
 
 
 ###########################################################
@@ -216,6 +224,7 @@ l_eye_tex_dir = 'textured data/l_eye'
 r_eye_tex_dir = 'textured data/r_eye'
 mouth_tex_dir = 'textured data/mouth'
 real_tex_dir = 'textured data/real'
+full_dir = 'textured data/full'
 
 light_folder='light_data/example_light/'
 model_folder='trained_model/'
@@ -274,6 +283,7 @@ def get_segmentation_map(i = 0, imgs = None, mode = 'auto', skin_path = '', hair
 
 def infuse_textures(seg_string = '', mode = 'auto', skin_tex_path = '', hair_tex_path = '', l_eye_tex_path = '',r_eye_tex_path = '', mouth_tex_path= '', seg_map_path = ''):
     if mode == 'auto':
+        
 
         skin_dir_list = random.choice(os.listdir(skin_tex_dir))
         skin_tex_path = os.path.join(skin_tex_dir, skin_dir_list)
@@ -289,7 +299,10 @@ def infuse_textures(seg_string = '', mode = 'auto', skin_tex_path = '', hair_tex
         mouth_dir_list = random.choice(os.listdir(mouth_tex_dir))
         mouth_tex_path = os.path.join(mouth_tex_dir, mouth_dir_list)
 
-        seg_map_path = seg_string
+        seg_dir_list = random.choice(os.listdir(full_dir))
+        seg_map_path = os.path.join(full_dir, seg_dir_list)
+        # seg_map_path = seg_string
+        
     elif mode == 'manual':
         skin_tex_path = skin_tex_path
         hair_tex_path = hair_tex_path
@@ -298,15 +311,58 @@ def infuse_textures(seg_string = '', mode = 'auto', skin_tex_path = '', hair_tex
         mouth_tex_path = mouth_tex_path
         seg_map_path = seg_string
 
+    imgs = []
     seg_map = preprocess(seg_map_path, 256).to(device)
     skin = preprocess(skin_tex_path, 256).to(device)
+    imgs.append(skin)
+
     hair = preprocess(hair_tex_path, 256).to(device)
+    imgs.append(hair)
+
     left_eye = preprocess(l_eye_tex_path, 256).to(device)
+    imgs.append(left_eye)
+
     right_eye = preprocess(r_eye_tex_path, 256).to(device)
+    imgs.append(right_eye)
+
     mouth = preprocess(mouth_tex_path, 256).to(device)
+    imgs.append(mouth)
+
 
     with torch.no_grad():
-        textured_image = texture_model(seg_map, skin, hair, left_eye, right_eye, mouth)
+        textured_image = texture_model(seg_map, imgs)
+
+    min_val = textured_image.min()
+    max_val = textured_image.max()
+    textured_image = (textured_image - min_val) / (max_val - min_val)
+    textured_image = textured_image.squeeze()
+    return seg_map, imgs, TF.to_pil_image(textured_image)
+
+def change_mouth_or_hair(seg_map, imgs):
+
+    random_number = random.randint(1, 3)
+    imgs = imgs
+    if random_number==1:
+        mouth_dir_list = random.choice(os.listdir(mouth_tex_dir))
+        mouth_tex_path = os.path.join(mouth_tex_dir, mouth_dir_list)
+        mouth = preprocess(mouth_tex_path, 256).to(device)
+        imgs[4] = mouth
+    if random_number==2:
+        hair_dir_list = random.choice(os.listdir(hair_tex_dir))
+        hair_tex_path = os.path.join(hair_tex_dir, hair_dir_list)
+        hair = preprocess(hair_tex_path, 256).to(device)
+        imgs[1] = hair
+    if random_number==3:
+        mouth_dir_list = random.choice(os.listdir(mouth_tex_dir))
+        mouth_tex_path = os.path.join(mouth_tex_dir, mouth_dir_list)
+        mouth = preprocess(mouth_tex_path, 256).to(device)
+        imgs[4] = mouth
+        hair_dir_list = random.choice(os.listdir(hair_tex_dir))
+        hair_tex_path = os.path.join(hair_tex_dir, hair_dir_list)
+        hair = preprocess(hair_tex_path, 256).to(device)
+        imgs[1] = hair
+    with torch.no_grad():
+        textured_image = texture_model(seg_map, imgs)
 
     min_val = textured_image.min()
     max_val = textured_image.max()
@@ -314,6 +370,57 @@ def infuse_textures(seg_string = '', mode = 'auto', skin_tex_path = '', hair_tex
     textured_image = textured_image.squeeze()
     return TF.to_pil_image(textured_image)
 
+
+def celeba():
+    for i, full in enumerate(os.listdir(full_dir)):
+        seg_map_path = os.path.join(full_dir, full)
+
+        skin_tex_path = seg_map_path.replace("full", "skin")
+        hair_tex_path = seg_map_path.replace("full", "hair")
+        l_eye_tex_path = seg_map_path.replace("full", "l_eye")
+        r_eye_tex_path = seg_map_path.replace("full", "r_eye")
+        mouth_tex_path = seg_map_path.replace("full", "mouth")
+
+
+        imgs = []
+        seg_map = preprocess(seg_map_path, 256).to(device)
+        skin = preprocess(skin_tex_path, 256).to(device)
+        imgs.append(skin)
+
+        hair = preprocess(hair_tex_path, 256).to(device)
+        imgs.append(hair)
+
+        left_eye = preprocess(l_eye_tex_path, 256).to(device)
+        imgs.append(left_eye)
+
+        right_eye = preprocess(r_eye_tex_path, 256).to(device)
+        imgs.append(right_eye)
+
+        mouth = preprocess(mouth_tex_path, 256).to(device)
+        imgs.append(mouth)
+
+
+        with torch.no_grad():
+            textured_image = texture_model(seg_map, imgs)
+        min_val = textured_image.min()
+        max_val = textured_image.max()
+        textured_image = (textured_image - min_val) / (max_val - min_val)
+        textured_image = textured_image.squeeze()
+        textured_image =  TF.to_pil_image(textured_image)
+
+
+        image = enhance(textured_image)
+        os.makedirs('celeba_syn', exist_ok=True)
+        new = full.split('_full.png')
+        string = 'celeba_syn/' + str(int(new[0])) + '.jpg'
+        image.save(string)
+        print(i)
+
+
+
+    
+
+    
 
 def inversion(image, truncation):
     image = transform(image).unsqueeze(0).to(device)
@@ -401,10 +508,9 @@ def pose_transfer(image, mode = 'auto', driving = ''):
     return eval(output = '', source = image,driving = driving)
 
 
-
 def age_changer(image, mode = 'auto', age = 0):
     if mode == 'auto':
-        target_ages = [ 20, 30, 40, 50, 60, 70, 80]
+        target_ages = [ 20, 30, 40, 50, 60]
 
         age = random.choice(target_ages)
     elif mode == 'manual':
@@ -419,6 +525,7 @@ def age_changer(image, mode = 'auto', age = 0):
         result_tensor = run_on_batch(input_image_age, net)[0]
         return tensor2im(result_tensor)
 
+
 def save_and_crop(image, uncropped, cropped):
 
     image.save(uncropped)
@@ -431,23 +538,34 @@ def save_and_crop(image, uncropped, cropped):
 
 
 
+#CUDA_VISIBLE_DEVICES=0 python generate.py
+#CUDA_VISIBLE_DEVICES=1 python generate.py
+#CUDA_VISIBLE_DEVICES=2 python generate.py
+#CUDA_VISIBLE_DEVICES=3 python generate.py
 
+cuda_mode = 3
+repeat = 1
 #Automated generation example
 def generate():
     mode = 'auto'
     for j, imgs in enumerate(os.listdir('segmentation data/skin')):
-        if j < 0:
+        if j < 7500 * cuda_mode:
             pass
-        # elif j == 7500:
-        #     break
+        elif j == 7500 * (cuda_mode + 1):
+            break
         else:
-            i = j + (30000 * 0)
+            i = j + (30000 * repeat)
             uncropped_folder, cropped_folder = create_folders(i)
             seg_string = get_segmentation_map(i, imgs, mode = 'auto')
-            textured_image = infuse_textures(seg_string, mode = 'auto')
-            style_image = inversion(textured_image, 0.6)
+            seg_map, images, base_texture = infuse_textures(seg_string, mode = 'auto')
+            # style_image = inversion(textured_image, 0.6)
             for p in range(100):
-                light_image = relighting(style_image)
+                random_number = random.randint(1, 2)
+                if random_number == 1:
+                    textured_image = base_texture
+                elif random_number == 2:
+                    textured_image = change_mouth_or_hair(seg_map, images)
+                light_image = relighting(textured_image)
                 new_pose = pose_transfer(light_image, mode = 'auto')
                 image = Image.fromarray(new_pose.astype('uint8'), 'RGB')
                 image = enhance(image)
@@ -467,44 +585,148 @@ def manual():
     seg_string = get_segmentation_map(mode = 'manual', skin_path=skin_path, hair_path=hair_path, mouth_path=mouth_path)
     
     skin_tex_path = 'textured data/skin/00001_skin.png'
-    hair_tex_path = 'textured data/hair/00005_hair.png'
-    l_eye_tex_path = 'textured data/l_eye/00028_l_eye.png'
-    r_eye_tex_path = 'textured data/r_eye/00039_r_eye.png'
-    mouth_tex_path = 'textured data/mouth/00045_mouth.png'
-    seg_map_path = 'textured data/full/00017_full.png'
+    hair_tex_path = 'textured data/hair/07682_hair.png'
+    l_eye_tex_path = 'textured data/l_eye/09872_l_eye.png'
+    r_eye_tex_path = 'textured data/r_eye/02131_r_eye.png'
+    mouth_tex_path = 'textured data/mouth/00981_mouth.png'
+    seg_map_path = 'textured data/full/00001_full.png'
     #or use seg string from the previously generated segmentation map
 
-    textured_image = infuse_textures(mode = 'manual',skin_tex_path=skin_tex_path, hair_tex_path=hair_tex_path, l_eye_tex_path=l_eye_tex_path, r_eye_tex_path=r_eye_tex_path, mouth_tex_path=mouth_tex_path,seg_string=seg_map_path )
-    textured_image.save('test/textured.jpg')
+    _,_,textured_image = infuse_textures(mode = 'manual',skin_tex_path=skin_tex_path, hair_tex_path=hair_tex_path, l_eye_tex_path=l_eye_tex_path, r_eye_tex_path=r_eye_tex_path, mouth_tex_path=mouth_tex_path,seg_string=seg_map_path )
+    textured_image.save('test/textured1.jpg')
+    # exit()
 
-    style_image = inversion(textured_image, 0.6)
-    style_image.save('test/style.jpg')
+    # style_image = inversion(textured_image, 0.6)
+    # style_image.save('test/style.jpg')
 
-    light_image = relighting(style_image, mode = 'manual', loc = 1)
-    light_image.save('test/light.jpg')
+    light_image = relighting(textured_image, mode = 'manual', loc = 1)
+    light_image.save('test/light1.jpg')
 
     driving = 'textured data/real/0.jpg'
 
     new_pose = pose_transfer(light_image, mode = 'manual', driving = driving)
     new_pose = Image.fromarray(new_pose.astype('uint8'), 'RGB')
 
-    new_pose.save('test/pose.jpg')
+    new_pose.save('test/pose1.jpg')
 
 
     enhanced = enhance(new_pose)
-    enhanced.save('test/enhanced.jpg')
+    enhanced.save('test/enhanced1.jpg')
     
     new_age = transform(enhanced)
-    new_age = age_changer(new_age, mode = 'manual', age = 40)
-    new_age.save('test/age.jpg')
+    new_age = age_changer(new_age, mode = 'manual', age = 20)
+    new_age.save('test/age1.jpg')
 
+def lighting_change():
+    mode = 'manual'
+    skin_tex_path = 'textured data/skin/00001_skin.png'
+    hair_tex_path = 'textured data/hair/00001_hair.png'
+    l_eye_tex_path = 'textured data/l_eye/00001_l_eye.png'
+    r_eye_tex_path = 'textured data/r_eye/00001_r_eye.png'
+    mouth_tex_path = 'textured data/mouth/00001_mouth.png'
+    seg_map_path = 'textured data/full/00001_full.png'
+    #or use seg string from the previously generated segmentation map
+
+    _,_,textured_image = infuse_textures(mode = 'manual',skin_tex_path=skin_tex_path, hair_tex_path=hair_tex_path, l_eye_tex_path=l_eye_tex_path, r_eye_tex_path=r_eye_tex_path, mouth_tex_path=mouth_tex_path,seg_string=seg_map_path )
+
+    style_image = inversion(textured_image, 0.6)
+    os.makedirs('slight_changes/light',exist_ok=True)
+
+    for i in range(20):
+        light_image = relighting(style_image, mode = 'auto')
+        light_string = 'slight_changes/light/' + str(i) +'.jpg'
+        light_image = enhance(light_image)
+        light_image.save(light_string)
+
+def pose_change():
+    mode = 'manual'
+    skin_tex_path = 'textured data/skin/00001_skin.png'
+    hair_tex_path = 'textured data/hair/00001_hair.png'
+    l_eye_tex_path = 'textured data/l_eye/00001_l_eye.png'
+    r_eye_tex_path = 'textured data/r_eye/00001_r_eye.png'
+    mouth_tex_path = 'textured data/mouth/00001_mouth.png'
+    seg_map_path = 'textured data/full/00001_full.png'
+    #or use seg string from the previously generated segmentation map
+
+    _,_,textured_image = infuse_textures(mode = 'manual',skin_tex_path=skin_tex_path, hair_tex_path=hair_tex_path, l_eye_tex_path=l_eye_tex_path, r_eye_tex_path=r_eye_tex_path, mouth_tex_path=mouth_tex_path,seg_string=seg_map_path )
+
+    style_image = inversion(textured_image, 0.6)
+    os.makedirs('slight_changes/pose',exist_ok=True)
+
+    for i in range(20):
+        new_pose = pose_transfer(style_image, mode = 'auto')
+        pose_image = Image.fromarray(new_pose.astype('uint8'), 'RGB')
+        pose_string = 'slight_changes/pose/' + str(i) +'.jpg'
+        pose_image = enhance(pose_image)
+        pose_image.save(pose_string)
+
+def age_change():
+    mode = 'manual'
+    skin_tex_path = 'textured data/skin/00001_skin.png'
+    hair_tex_path = 'textured data/hair/00001_hair.png'
+    l_eye_tex_path = 'textured data/l_eye/00001_l_eye.png'
+    r_eye_tex_path = 'textured data/r_eye/00001_r_eye.png'
+    mouth_tex_path = 'textured data/mouth/00001_mouth.png'
+    seg_map_path = 'textured data/full/00001_full.png'
+    #or use seg string from the previously generated segmentation map
+
+    _,_,textured_image = infuse_textures(mode = 'manual',skin_tex_path=skin_tex_path, hair_tex_path=hair_tex_path, l_eye_tex_path=l_eye_tex_path, r_eye_tex_path=r_eye_tex_path, mouth_tex_path=mouth_tex_path,seg_string=seg_map_path )
+
+    style_image = inversion(textured_image, 0.6)
+    os.makedirs('slight_changes/age',exist_ok=True)
+    style_image = transform(style_image)
+    ages = [10, 20, 30,40,50,60,70,80]
+    for i in ages:
+        new_age = age_changer(style_image, mode = 'manual', age = i)
+        age_string = 'slight_changes/age/' + str(i) +'.jpg'
+        new_age.save(age_string)
+
+def slight_changes():
+    for i in range(100):
+        _,_,textured_image = infuse_textures(mode = 'auto')
+        style_image = inversion(textured_image, 0.6)
+
+        os.makedirs('slight_changes/{}/age'.format(str(i)),exist_ok=True)
+        os.makedirs('slight_changes/{}/pose'.format(str(i)),exist_ok=True)
+        os.makedirs('slight_changes/{}/light'.format(str(i)),exist_ok=True)
+
+
+
+        for j in range(10):
+            new_pose = pose_transfer(style_image, mode = 'auto')
+            pose_image = Image.fromarray(new_pose.astype('uint8'), 'RGB')
+            pose_string = 'slight_changes/{}/pose/'.format(str(i)) + str(j) +'.jpg'
+            pose_image = enhance(pose_image)
+            pose_image.save(pose_string)
+        print(i, ': Pose')
+
+        for j in range(7):
+            light_image = relighting(style_image, mode = 'manual', loc=j)
+            light_string = 'slight_changes/{}/light/'.format(str(i)) + str(j) +'.jpg'
+            light_image = enhance(light_image)
+            light_image.save(light_string)
+        print(i, ': Light')
+            
+        style_image = transform(style_image)
+        ages = [10, 20, 30,40,50,60,70,80]
+        for j in ages:
+            new_age = age_changer(style_image, mode = 'manual', age = j)
+            age_string = 'slight_changes/{}/age/'.format(str(i)) + str(j) +'.jpg'
+            new_age.save(age_string)
+
+        print(i, ': Age')
 
 
 
 if __name__ == "__main__":
-    #generate()
+    # generate()
     # manual()
-    pass
+    # lighting_change()
+    # pose_change()
+    # age_change()
+    # slight_changes()
+    celeba()
+
 
 
 
